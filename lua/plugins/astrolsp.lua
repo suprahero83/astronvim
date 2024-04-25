6e3b2ab0a767971bc329936d7e438aff3b1b7c71
suprahero83 <78385525+suprahero83@users.noreply.github.com>
2024-04-25 14:43:15 -0700

    Update astrolsp.lua setup for PS

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
 -- Configuration documentation can be found with `:h astrolsp`
@@ -41,11 +41,17 @@ return {
     -- enable servers that you already have installed without mason
     servers = {
       -- "pyright"
+      "powershell_es"
     },
     -- customize language server configuration options passed to `lspconfig`
     ---@diagnostic disable: missing-fields
     config = {
       -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
+      powershell_es = {
+      bundle_path = 'c:/w/PowerShellEditorServices',
+      settings = { powershell = { codeFormatting = { Preset = 'OTBS' } } }
+      }
+
     },
     -- customize how language servers are attached
     handlers = {
